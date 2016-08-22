6196807d7359d59ed8b5b836c105837c23b52722
CJM <cj.mccarey@gmail.com>
2016-08-21 21:24:28 -0700

    Initial Commit
    Completed initial testing for connecting into vcenter and migrating a VM
    between all powered on hosts.

---
+++ testvmnetwork.ps1
@@ -0,0 +1,14 @@
+﻿<#
+    Get all hosts and move the specified VM to each host
+#>
+$vcenter = "GAM-VCENTER-01.grcadvantage.com"
+$vmname = "GAM-TEST-01"
+
+Connect-VIserver $vcenter
+$hosts = Get-VMHost | where {$_.PowerState -eq "PoweredOn" }
+$vm = Get-VM -Name $vmname
+foreach ($objHost in $hosts) {
+    Move-VM -VM $vm -Destination (Get-VMHost $objHost.name)
+    Start-Sleep -Seconds 8
+}
+# Get-VM -Location "HostName" | Move-VM -Destination (Get-Vmhost "NewHost")
