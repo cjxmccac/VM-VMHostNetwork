a3d11da907f93404591c7b75f4c004c31bbbc949
CJM <cj.mccarey@gmail.com>
2016-08-26 12:22:48 -0700

    Add Cluster Filter
    Added cluster variable so hosts can be pulled from specific clusters.

--- testvmnetwork.ps1
+++ testvmnetwork.ps1
@@ -2,10 +2,11 @@
     Get all hosts and move the specified VM to each host
 #>
 $vcenter = "GAM-VCENTER-01.grcadvantage.com"
-$vmname = "GAM-TEST-01"
+$cluster = "LBC Gaming Cluster"
+$vmname = "LBC-GAMTEST-01"
 
 Connect-VIserver $vcenter
-$hosts = Get-VMHost | where {$_.PowerState -eq "PoweredOn" }
+$hosts = Get-Cluster -Name $cluster | Get-VMHost | where {$_.PowerState -eq "PoweredOn" }
 $vm = Get-VM -Name $vmname
 foreach ($objHost in $hosts) {
     Move-VM -VM $vm -Destination (Get-VMHost $objHost.name)
